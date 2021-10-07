0dc828a506511046d0a723f5be9fa3d3391d5da7
kumara <kumara@MININT-DDPI>
2021-10-07 10:18:22 -0700

    Add project files.

---
+++ 1Destiny/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace _1Destiny.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        
+
+        public ActionResult SDK()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Resources()
+        {
+            ViewBag.Message = "Your Resource page";
+
+            return View();
+        }
+        public ActionResult Tools()
+        {
+            ViewBag.Message = "Your Resource page";
+
+            return View();
+        }
+        public void QuickAssist()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "C:\\Windows\\System32\\quickassist.exe";
+            p.Start();
+        }
+        
+    }
+}
---
+++ 1Destiny/Models/RichTextEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using System.Web.Mvc;
+namespace RichTextEditor.Models
+{
+    public class RichTextEditorViewModel
+    {
+        [AllowHtml]
+        [Display(Name = "Here goes the case notes.")]
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+}
